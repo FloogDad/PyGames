7c3eb6a92bed841c49095663593d104c276aa27a
FloogDad <139724367+FloogDad@users.noreply.github.com>
2023-07-17 11:42:28 +1000

    Revert "Update 99bottles.py"
    This reverts commit 60e562a35cc9166c1710a9b29035f8e83fb3e3a5.

--- 99bottles.py
+++ 99bottles.py
@@ -1,2 +1,2 @@
 for i in range(99, 0, -1):
-    print(str(i)+(" bottle" if i == 1 else " bottles") +" of juice on the wall, "+str(i)+(" bottle" if i == 1 else " bottles") +" of juice", "+str(i))
+    print(str(i)+(" bottle" if i == 1 else " bottles") +" of rum, "+str(i)+", "+str(i))
