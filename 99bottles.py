c46797b7518ff0487baad37b286deb9e50bf6387
FloogDad <139724367+FloogDad@users.noreply.github.com>
2023-07-19 20:42:59 +1000

    Updated 99 bottles

--- 99bottles.py
+++ 99bottles.py
@@ -1,2 +1,3 @@
 for i in range(99, 0, -1):
     print(str(i)+(" bottle" if i == 1 else " bottles") +" of juice on the wall, "+str(i)+(" bottle" if i == 1 else " bottles") +" of juice.")
+    print("Take one down and pass it around, "+str(i-1)+ (" bottle" if i == 2 else " bottles") +" of juice on the wall.\n")
