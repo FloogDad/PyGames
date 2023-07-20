f632f7dac7bfa631915479bee9a358f38c45a842
FloogDad <byronarcia@gmail.com>
2023-07-21 00:15:20 +1000

    more pauses

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -6,33 +6,40 @@ scoreboard = {
 }
 def script():
 	while True:
-		playrsp = input("Pick rock, paper or scissors \n")
+		playrsp = input("Type rock, paper or scissors \n")
 		if playrsp in ("rock","paper","scissors"):
 			break
-		print("only type rock, paper or scissors, try again")
+		print("Only type rock, paper or scissors, try again")
 		time.sleep(1)
 	complist = ["rock","paper","scissors"]
 	playlist = ["paper","scissors","rock"]
 	comprsp = (random.choice(complist))
-	print("computer picked "+comprsp)
+	print("computer held out "+comprsp)
+	time.sleep(0.5)
 	if complist.index(comprsp) == playlist.index(playrsp):
 		print("You win!")
 		scoreboard["player"]+=1
+		time.sleep(0.5)
 	elif comprsp  == playrsp:
 		print("It's a Tie!")
 	else:
 		print("You lose!")
 		scoreboard["computer"]+=1
+		time.sleep(0.5)
 	while True:
 		for k,v in scoreboard.items():
-			print(str(k)+" score = "+str(v))
+			print(k+"'s score = "+str(v))
+			time.sleep(0.5)
 		restart = input("Do you want to play again? (yes/no)")
-		if restart in ('yes,''y'):
+		if restart in ('yes','y'):
 			script()
 			break
 		if restart in ('no','n'):
 			time.sleep(1)
-			print("Thanks for playing. Bye")
+			print("Thanks for playing.")
+			time.sleep(0.5)
+			bye = 'Bye.\n' 
+			for i in range(5): print(bye[i], sep='', end='', flush=True); time.sleep(0.3)
 			time.sleep(1)
 			break
 		print("type yes or no")
